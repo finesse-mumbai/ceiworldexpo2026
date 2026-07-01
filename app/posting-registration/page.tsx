1ab01fea093919373281d301994ee9622b87b6c8
snehasabariya <developer@finesse.com>
2026-07-01 17:25:14 +0530

    Integrate Posting Registration buyer page

---
+++ app/posting-registration/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from 'react';
+import BuyerRegistrationForm from '../components/BuyerRegistrationForm';
+
+export default function PostingRegistrationPage() {
+  return <BuyerRegistrationForm defaultUtmSource="Posting_Registration" />;
+}
