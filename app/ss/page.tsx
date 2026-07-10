7d8ab5d0aab5f44e42431519f0a5181560fa7a2c
Sneha Sabariya <snehasabariya1997@gmail.com>
2026-07-10 11:29:57 +0530

    Add new registration source routes: dl, mu, ss

---
+++ app/ss/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from 'react';
+import BuyerRegistrationForm from '../components/BuyerRegistrationForm';
+
+export default function SSRegistrationPage() {
+  return <BuyerRegistrationForm defaultUtmSource="ss" />;
+}
