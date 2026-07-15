a11993a2b3dde2354aa202a8e31888cc0741197e
sneha sabariya <snehasabariya1997@gmail.com>
2026-07-15 15:48:35 +0530

    icea logo addition

--- app/components/Partners.tsx
+++ app/components/Partners.tsx
@@ -18,7 +18,8 @@ export default function Partners() {
       "/images/partners/Supporting-Associations/cmai.png",
       "/images/partners/Supporting-Associations/adcta.png",
       "/images/partners/Supporting-Associations/faiita.png",
-      "/images/partners/Supporting-Associations/iesa.png"
+      "/images/partners/Supporting-Associations/iesa.png",
+      "/images/partners/Supporting-Associations/icea.png"
     ],
     media: [
       { src: "/images/partners/Supporting-Media-Partners/Electronics-Era-logo.png", href: "https://electronicsera.in/" },
