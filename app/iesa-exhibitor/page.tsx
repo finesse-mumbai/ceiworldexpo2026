069bef769867138fa868031367449dd912c21c4d
sneha sabariya <snehasabariya1997@gmail.com>
2026-07-15 17:11:28 +0530

    exhibitor registration form addition

---
+++ app/iesa-exhibitor/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from 'react';
+import ExhibitorRegistrationForm from '../components/ExhibitorRegistrationForm';
+
+export default function IESAExhibitorRegistrationPage() {
+  return <ExhibitorRegistrationForm defaultSource="IESA" />;
+}
