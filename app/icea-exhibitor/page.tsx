72da651333478093cd3c82d3896644d5c8aaffd3
sneha sabariya <snehasabariya1997@gmail.com>
2026-07-17 10:56:19 +0530

    icea exhibitor registration page

---
+++ app/icea-exhibitor/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from 'react';
+import ExhibitorRegistrationForm from '../components/ExhibitorRegistrationForm';
+
+export default function ICEAExhibitorRegistrationPage() {
+  return <ExhibitorRegistrationForm defaultSource="ICEA" />;
+}
