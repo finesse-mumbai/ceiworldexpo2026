cb70336f4999a4b299d4866ca60b2a1f93920c31
sneha sabariya <snehasabariya1997@gmail.com>
2026-07-21 11:17:16 +0530

    vip-buyer addition package addition

---
+++ app/vip-buyer/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from 'react';
+import BuyerRegistrationForm from '../components/BuyerRegistrationForm';
+
+export default function VipBuyerRegistrationPage() {
+  return <BuyerRegistrationForm defaultUtmSource="vip-buyer" />;
+}
