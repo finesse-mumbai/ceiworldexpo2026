b08dea52b304f97b412e2ecbd662846e773f92a9
sneha sabariya <snehasabariya1997@gmail.com>
2026-07-25 16:22:02 +0530

    ine buyer registration form

---
+++ app/ine/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from 'react';
+import BuyerRegistrationForm from '../components/BuyerRegistrationForm';
+
+export default function IneBuyerRegistrationPage() {
+  return <BuyerRegistrationForm defaultUtmSource="ine" />;
+}
