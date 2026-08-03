2626c44356b2db714c8cec206b5bbfaaf9589ba9
sneha sabariya <snehasabariya1997@gmail.com>
2026-08-03 18:04:33 +0530

    buyer registration form 3 different source link update

---
+++ app/nbt/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from 'react';
+import BuyerRegistrationForm from '../components/BuyerRegistrationForm';
+
+export default function NbtBuyerRegistrationPage() {
+  return <BuyerRegistrationForm defaultUtmSource="nbt" />;
+}
