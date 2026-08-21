dd7533e5d305f89c5be3d3c7313c93e6e9aad5c4
sneha sabariya <snehasabariya1997@gmail.com>
2026-08-21 16:45:16 +0530

    scrolltotop update

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import PastEdition from "./components/PastEdition";
 import Partners from "./components/Partners";
 import ContactSection from "./components/ContactSection";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
       <Partners />
       <ContactSection />
       <Footer />
+      <ScrollToTop />
     </>
   );
 }
